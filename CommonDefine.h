ba1b27dcbc54b567c215366cd8102e5025fcbe50
黄人煌 <MyData@MacBookPro.local>
2018-03-18 20:41:12 +0800

    commit

---
+++ CommonDefine.h
@@ -0,0 +1,18 @@
+//
+// Created by 黄人煌 on 2018/3/18.
+//
+
+#ifndef KYDS_COMMONDEFINE_H
+#define KYDS_COMMONDEFINE_H
+
+#define TRUE        1
+#define FALSE       0
+#define OK          1
+#define ERROR       0
+#define INFEASIBLE  -1
+#define OVERFLOW    -2
+typedef int Status;
+typedef int Bool;
+
+#endif //KYDS_COMMONDEFINE_H
+
