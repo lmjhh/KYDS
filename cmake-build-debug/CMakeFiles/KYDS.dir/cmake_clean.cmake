4e3e93bff9be6dde10ecc55c04023dc8edd19b76
黄人煌 <huangrenhuang@huangrenhuangdeMac-mini.local>
2018-03-18 17:00:43 +0800

    Commit

---
+++ cmake-build-debug/CMakeFiles/KYDS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KYDS.dir/main.cpp.o"
+  "KYDS.pdb"
+  "KYDS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KYDS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
